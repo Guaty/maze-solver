5c5cc9a2618905591ea8d10d4cee4e25ee47d75c
Guaty <bjarocha911@gmail.com>
2024-12-23 22:29:52 -0600

    Add cell class

--- graphics.py
+++ graphics.py
@@ -4,7 +4,7 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("That last title was rude, I'm sorry")
+        self.__root.title("Normal Title")
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack()
         self.__running = False
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(3, 250), Point(526, 480))
-    l2 = Line(Point(367, 400), Point(400, 300))
-    win.draw_line(l, "blue")
-    win.draw_line(l2)
+    
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
