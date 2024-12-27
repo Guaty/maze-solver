0880dec6d2b3b936d699a1739e432e6413d59aaf
Guaty <bjarocha911@gmail.com>
2024-12-26 18:13:15 -0600

    Add break_walls_r with tests

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 from cell import Cell
 import time
+import random
 
 
 class Maze:
@@ -12,6 +13,7 @@ class Maze:
             cell_size_x,
             cell_size_y,
             win=None,
+            seed=None
         ):
         self._x1 = x1
         self._y1 = y1
@@ -20,6 +22,8 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
 
         self._cells = self._create_cells()
 
@@ -28,6 +32,7 @@ class Maze:
                 self._draw_cell(i,j)
 
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
 
     def _create_cells(self):
         return [[Cell(self._win) for _ in range(self._num_rows)] for _ in range(self._num_cols)]
@@ -37,6 +42,55 @@ class Maze:
         self._draw_cell(0, 0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+    
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+
+            #list cells to visit. remember: i = columns = horizontal, j = rows = vertical
+            #check left
+            if i > 0 and not self._cells[i-1][j].visited:
+                to_visit.append((i-1, j))
+            #check right
+            if i < self._num_cols-1 and not self._cells[i+1][j].visited:
+                to_visit.append((i+1, j))
+            #check up
+            if j > 0 and not self._cells[i][j-1].visited:
+                to_visit.append((i, j-1))
+            #check down
+            if j < self._num_rows-1 and not self._cells[i][j+1].visited:
+                to_visit.append((i, j+1))
+            
+            #if nowhere to go, break out; we're done
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            #pick a direction, then set a destination
+            next_direction = random.randrange(len(to_visit))
+            next_destination = to_visit[next_direction]
+
+            #break some walls. remember rules above
+            #left
+            if next_destination[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i-1][j].has_right_wall = False
+            #right
+            if next_destination[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i+1][j].has_left_wall = False
+            #up
+            if next_destination[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j-1].has_bottom_wall = False
+            #down
+            if next_destination[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j+1].has_top_wall = False
+
+            #move to the next cell
+            self._break_walls_r(next_destination[0], next_destination[1])
 
     def _draw_cell(self, i, j):
         if self._win is None:
--- tests.py
+++ tests.py
@@ -41,7 +41,67 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
             False,
         )
+    
+    def test_maze_generation(self):
+        maze1 = Maze(10, 10, 5, 5, 2, 2, seed=42)
+        maze2 = Maze(10, 10, 5, 5, 2, 2, seed=42)
         
+        for i in range(maze1._num_cols):
+            for j in range(maze1._num_rows):
+                cell1 = maze1._cells[i][j]
+                cell2 = maze2._cells[i][j]
+                
+                assert cell1.visited == cell2.visited
+                assert cell1.has_left_wall == cell2.has_left_wall
+                assert cell1.has_right_wall == cell2.has_right_wall
+                assert cell1.has_top_wall == cell2.has_top_wall
+                assert cell1.has_bottom_wall == cell2.has_bottom_wall
+
+    def test_valid_maze_cells(self):
+        maze = Maze(0,0, 12, 10, 5, 5, seed=42)
+        for i in range(maze._num_cols):
+            for j in range(maze._num_rows):
+                cell = maze._cells[i][j]
+
+                one_wall_present = (
+                    cell.has_bottom_wall or
+                    cell.has_left_wall or
+                    cell.has_right_wall or
+                    cell.has_top_wall
+                )
+
+                all_walls_present = (
+                    cell.has_bottom_wall and
+                    cell.has_left_wall and
+                    cell.has_right_wall and
+                    cell.has_top_wall
+                )
+
+                self.assertTrue(one_wall_present, f"Cell at {i},{j} has no walls!")
+                self.assertFalse(all_walls_present, f"Cell at {i},{j} is completely surrounded!")
+
+    def test_adjacent_cell_walls_match(self):
+        maze = Maze(0,0, 12, 10, 5, 5, seed=42)
+        for i in range(maze._num_cols):
+            for j in range(maze._num_rows):
+                cell = maze._cells[i][j]
+
+                if i > 0:
+                    left_cell = maze._cells[i-1][j]
+                    self.assertEqual(cell.has_left_wall, left_cell.has_right_wall)
+
+                if i < maze._num_cols - 1:
+                    right_cell = maze._cells[i+1][j]
+                    self.assertEqual(cell.has_right_wall, right_cell.has_left_wall)
+
+                if j > 0:
+                    top_cell = maze._cells[i][j-1]
+                    self.assertEqual(cell.has_top_wall, top_cell.has_bottom_wall)
+
+                if j < maze._num_rows - 1:
+                    bottom_cell = maze._cells[i][j+1]
+                    self.assertEqual(cell.has_bottom_wall, bottom_cell.has_top_wall)
+    
 
 if __name__ == "__main__":
     unittest.main()
